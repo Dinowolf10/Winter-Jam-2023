f22db2770d48172eaa56d9e239e068114a4cf8c4
Zaven <dinowolf10@gmail.com>
2023-01-13 19:28:22 -0500

    Implemented jump and refactored movement to rigidbody

--- Winter Jam 2023/Assets/Scripts/Player.cs
+++ Winter Jam 2023/Assets/Scripts/Player.cs
@@ -5,11 +5,22 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Vector2 moveVector;
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private Vector2 moveVelocity;
     [SerializeField]
     private float xMovement = 0f;
     [SerializeField]
     private float moveSpeed = 5f;
+
+    [SerializeField]
+    private bool isJumping = false;
+    [SerializeField]
+    private bool canJump = false;
+    [SerializeField]
+    private Vector2 jumpForce;
 
     // Start is called before the first frame update
     private void Start()
@@ -20,7 +31,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        transform.Translate(new Vector2(xMovement, 0) * moveSpeed * Time.deltaTime);
+        // Update jump variables
+        if (rb.velocity.y == 0f)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move player
+        rb.velocity = new Vector2(xMovement * moveSpeed, rb.velocity.y);
     }
 
     /// <summary>
@@ -30,5 +55,32 @@ public class Player : MonoBehaviour
     public void OnMovement(InputValue value)
     {
         xMovement = value.Get<float>();
+    }
+
+    /// <summary>
+    /// Performs a jump for the player
+    /// </summary>
+    /// <param name="value"></param>
+    public void OnJump(InputValue value)
+    {
+        // Only jump if the player is not currently jumping and if the player can jump (touching ground)
+        if (!isJumping && canJump)
+        {
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the player is jumping and collides with the floor, the current jump is over
+        if (isJumping)
+        {
+            if (collision.gameObject.tag == "Floor")
+            {
+                isJumping = false;
+            }
+        }
     }
 }
