329f47ff5f4fc121473d01e91077248ef8c9b789
natedog334 <58520743+natedog334@users.noreply.github.com>
2023-01-14 11:47:11 -0500

    Added bat prefab and refactored enemy movement script

--- Winter Jam 2023/Assets/Scripts/Enemy.cs
+++ Winter Jam 2023/Assets/Scripts/Enemy.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundEnemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
     // speed at which the enemy moves
     [SerializeField]
     private float moveSpeed;
+
+    // magnitude of the arc made by the enemy during movement (primarily for bats)
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float arcMagnitude;
+
+    // enemy movement vector with the arc applied
+    private Vector2 arcVector;
 
     // locations the the enemy patrols between
     [SerializeField]
@@ -24,7 +32,10 @@ public class GroundEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // move the enemy toward the next move spot and apply the desired arc amount
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[moveSpotIdx].position, moveSpeed * Time.deltaTime);
+        arcVector = new Vector2(transform.position.x, transform.position.y + (Mathf.Sin(Time.deltaTime) * arcMagnitude));
+        transform.position = arcVector;
 
         // if the enemy has reached a move spot, update it to move toward the next move spot
         if (Vector2.Distance(transform.position, moveSpots[moveSpotIdx].position) < 0.1f)
