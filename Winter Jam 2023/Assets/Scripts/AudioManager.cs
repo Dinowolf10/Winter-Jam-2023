9f730c35dc885b1c72a9c39e28638a078bc5c8e2
natedog334 <58520743+natedog334@users.noreply.github.com>
2023-01-14 16:45:09 -0500

    AudioManager now persists between scenes

--- Winter Jam 2023/Assets/Scripts/AudioManager.cs
+++ Winter Jam 2023/Assets/Scripts/AudioManager.cs
@@ -14,6 +14,8 @@ public class AudioManager : MonoBehaviour
 
     void Awake()
     {
+        DontDestroyOnLoad(this);
+
         // ensure only one AudioManager exists in a scene
         if (instance == null)
         {
