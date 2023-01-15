0378cd8a375202d2646df5e33035d93c07ea7a11
natedog334 <58520743+natedog334@users.noreply.github.com>
2023-01-15 14:17:10 -0500

    Adjusted main camera to follow player

--- Winter Jam 2023/Assets/Scripts/FollowPlayer.cs
+++ Winter Jam 2023/Assets/Scripts/FollowPlayer.cs
@@ -8,39 +8,62 @@ using UnityEngine;
 /// </summary>
 public class FollowPlayer : MonoBehaviour
 {
-    // reference to player object
+    // x-axis camera offset
     [SerializeField]
-    private GameObject player;
+    private float xOffset;
 
-    // amount the camera shows ahead of the player
+    // y-axis camera offset
     [SerializeField]
-    private float offset;
+    private float yOffset;
+
+    [SerializeField]
+    private float maxYThreshold;
+
+    [SerializeField]
+    private float minYThreshold;
 
     // speed at which the camera moves toward its offset position
     [SerializeField]
     private float offsetSmoothing;
 
+    private GameObject player;
     private Vector3 playerPosition;
     private SpriteRenderer playerSpriteRenderer;
+    private float playerY;
 
     private void Start()
     {
+        player = GameObject.Find("Player");
         playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        // determine if the camera should move on the y-axis or not
+        playerY = player.transform.position.y;
+        if (playerY > maxYThreshold)
+        {
+            playerPosition = new Vector3(player.transform.position.x, player.transform.position.y + yOffset, transform.position.z);
+        }
+        else if (playerY < minYThreshold)
+        {
+            playerPosition = new Vector3(player.transform.position.x, player.transform.position.y - yOffset, transform.position.z);
+        }
+        else
+        {
+            playerPosition = new Vector3(player.transform.position.x, 0, transform.position.z);
+        }
+        
 
         // calculate offset camera position
         if (playerSpriteRenderer.flipX == false)
         {
-            playerPosition = new Vector3(playerPosition.x + offset, playerPosition.y, transform.position.z);
+            playerPosition = new Vector3(playerPosition.x + xOffset, playerPosition.y, transform.position.z);
         }
         else
         {
-            playerPosition = new Vector3(playerPosition.x - offset, playerPosition.y, transform.position.z);
+            playerPosition = new Vector3(playerPosition.x - xOffset, playerPosition.y, transform.position.z);
         }
 
         // lerp camera toward its desired location
